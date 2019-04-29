950ff98a278c54e8dd63f8dc6f9288e0247019ae
Vinícius França <44382752+Vinicinn@users.noreply.github.com>
2019-04-29 19:31:11 -0300

    Criar Novo Registro

--- novo.php
+++ novo.php
@@ -4,22 +4,46 @@
     <meta charset="utf-8">
     <title>Novo Registro</title>
 </head>
-<body>
-    <h1> Novo Registro</h1>
+<body style="background-color:orange;">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        
+        <div class="container">
+        <div class="row">
+			<div class="col-md-5 mx-auto">
+			<div id="first">
+				<div class="myform form ">
+					 <div class="logo mb-3">
+						 <div class="col-md-12 text-center">
+                         <body style="background-color:#F78181;">
+
+                         <h1><strong><u>Novo Registro</strong></u></h1>
 
     <form action=salvar.php >
 	
-    <p>Nome</p>
+    <p><strong><u>Nome:</strong></u></p>
     <input type=text name=nome>
-	
-    <p>Data De Nascimento</p>
+	<p>
+    </P>  
+    <p><strong><u>Data De Nascimento:</strong></u></p>
     <input type=date name=idade>
-	
-	<p>Salario</p>
+	<p>
+    </P>  
+	<p><strong><u>Salario:</strong></u></p>
     <input type=number name=salario>
     <br><br>
-	
+	<p>
+    </P>   
     <input type=submit value=Salvar>
-    </form>
-</body>
+    </form>                      
+                      
+    </div>
+   
+</head>   
+</html>
+ </body>
 </html>
